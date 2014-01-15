e04a6b8f401f7eff4b84e9cd060a61246bd700b1
Han van der Veen <han@haneev.nl>
2014-01-15 13:06:10 +0100

    Init project Linker

---
+++ src/java/org/utwente/bigdata/Helper.java
@@ -0,0 +1,68 @@
+package org.utwente.bigdata;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Helper class that only parse the sentences
+ * @author Han van der Veen
+ */
+public class Helper {
+	/**
+	 * Pattern that matches the lists: a,b,c or z. 
+	 */
+    public static Pattern p = Pattern.compile("((?:[a-z0-9_\\-]{3,25}\\s*){1,3}+[,]\\s*){1,10}((?:[a-z0-9_\\-]{3,25}\\s*){1,3}(\\s+(?:and|or)\\s+)(?:[a-z0-9_\\-]{3,25}\\s*){1,3})", Pattern.CASE_INSENSITIVE);
+	
+    /**
+     * Parse the lists into Tuples
+     * @param html
+     * @return list of tuples
+     */
+    public List<Tuple<String, String>> parts(String html) {
+    	
+    	List<Tuple<String,String>> list = new ArrayList<Tuple<String,String>>();
+    	Matcher m = p.matcher(html);
+    	
+    	while(m.find()) {
+    		
+    		// items of the lists
+    		List<String> items = new ArrayList<String>(); 
+    		
+    		String all = m.group(0);
+    		String[] parts = all.split("([,;]|and|or)");
+    		
+    		// item to a lists
+    		for(String part : parts) {
+    			if(!part.trim().isEmpty())
+    				items.add(part.trim());
+    		}
+    	
+    		// Parse the combinations into Tuples. Only tuples with (a,b) and not (b,a) and only if a is not already parsed.
+    		List<String> parsed = new ArrayList<String>();
+    		for(String item : items) {
+    			for(String inner : items) {
+    				if(!item.equals(inner)) {
+    					String a,b;
+    					if(item.compareToIgnoreCase(inner) < 0) {
+    						a = item; b = inner;
+    					} else {
+    						b = inner; a = item;
+    					}
+    					
+    					// check if tuple already exists...
+    					if(!parsed.contains(a)) {
+    						Tuple<String, String> t = new Tuple<String, String>(a,b);    						    						
+    						list.add(t);
+    						parsed.add(a);
+    					}
+    				}
+    				
+    			}
+    		}
+    	}
+    	
+    	return list;
+    }
+}
---
+++ src/java/org/utwente/bigdata/Linker.java
@@ -0,0 +1,241 @@
+package org.utwente.bigdata;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.commoncrawl.examples.ExampleTextWordCount;
+import org.commoncrawl.examples.SampleFilter;
+import org.commoncrawl.hadoop.mapred.ArcInputFormat;
+import org.commoncrawl.hadoop.mapred.ArcRecord;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * An example showing how to analyze the Common Crawl ARC web content files.
+ * @author Chris Stephens <chris@commoncrawl.org>
+ * 
+ * Improved for Linker by
+ * @author Han van der Veen
+ */
+public class Linker extends Configured implements Tool {
+
+  private static final Logger LOG = Logger.getLogger(Linker.class);
+  private static final String ARGNAME_INPATH = "-in";
+  private static final String ARGNAME_OUTPATH = "-out";
+  private static final String ARGNAME_CONF = "-conf";
+  private static final String ARGNAME_OVERWRITE = "-overwrite";
+  private static final String ARGNAME_MAXFILES = "-maxfiles";
+  private static final String ARGNAME_NUMREDUCE = "-numreducers";
+  private static final String FILEFILTER = ".arc.gz";
+  
+  private static Helper h = new Helper();
+  
+  protected static enum MAPPERCOUNTER {
+    HTML_ERROR,
+    EXCEPTIONS,
+    OUT_OF_MEMORY,
+    DOCUMENTS_IN
+  };
+
+  /**
+   */
+  public static class LinkerMapper extends Mapper<Text, ArcRecord, Text, LongWritable> {
+    
+    private LongWritable outVal = new LongWritable(1);
+ 
+    // HTML to be parsed
+    private String html;
+    
+    public void map(Text key, ArcRecord value, final Context context) throws IOException {
+      try {
+    	// only get html
+        if (!value.getContentType().contains("html")) {
+          context.getCounter(MAPPERCOUNTER.HTML_ERROR).increment(1);
+          return;
+        }
+
+        // ensure sample instances have enough memory to parse HTML
+        if (value.getContentLength() > (1 * 1024 * 1024)) {
+          context.getCounter(MAPPERCOUNTER.HTML_ERROR).increment(1);
+          return;
+        }
+        
+        html = value.getHTML();
+        if(html == null) {
+       	 	context.getCounter(MAPPERCOUNTER.HTML_ERROR).increment(1);
+            return;
+        }
+        
+        // maximum time for executing a page is 200ms
+        Thread r = new Thread() {
+        	public void run() {
+        		
+        		try {
+        			context.getCounter(MAPPERCOUNTER.DOCUMENTS_IN).increment(1);
+        			
+        			// replace all html with nothing
+	                html = html.replaceAll("<[^>]+>", "");
+	                html = html.replaceAll("&[a-zA-Z]+;"," ");
+	                	
+	                // get a list of tuples of the html
+	                List<Tuple<String, String>> tuples = h.parts(html);
+	
+	                // parse it and emit them concatenated with ||
+                	for(Tuple<String, String> tuple : tuples) {
+                		String a = tuple.getFirst().trim();
+                		String b = tuple.getSecond().trim();
+                		if(b.length() > 3 && a.length() > 3) {
+                			context.write(new Text(a + "||" + b), outVal);
+                		}
+                	}
+        		} catch(Exception e) {
+        			 LOG.error("Caught Exception", e);
+        		     context.getCounter(MAPPERCOUNTER.EXCEPTIONS).increment(1);
+        		}
+        	}
+        };
+        
+       r.run();
+       r.join(200);
+        
+      }
+      catch (Throwable e) {
+        LOG.error("Caught Exception", e);
+        context.getCounter(MAPPERCOUNTER.EXCEPTIONS).increment(1);
+      }
+    }
+  }
+  
+  public void usage() {
+    System.out.println("\n  org.commoncrawl.examples.ExampleArcMicroformat \n" +
+                         "                           " + ARGNAME_INPATH +" <inputpath>\n" +
+                         "                           " + ARGNAME_OUTPATH + " <outputpath>\n" +
+                         "                         [ " + ARGNAME_OVERWRITE + " ]\n" +
+                         "                         [ " + ARGNAME_NUMREDUCE + " <number_of_reducers> ]\n" +
+                         "                         [ " + ARGNAME_CONF + " <conffile> ]\n" +
+                         "                         [ " + ARGNAME_MAXFILES + " <maxfiles> ]");
+    System.out.println("");
+    GenericOptionsParser.printGenericCommandUsage(System.out);
+  }
+
+  /**
+   * Implmentation of Tool.run() method, which builds and runs the Hadoop job.
+   *
+   * @param  args command line parameters, less common Hadoop job parameters stripped
+   *              out and interpreted by the Tool class.  
+   * @return      0 if the Hadoop job completes successfully, 1 if not. 
+   */
+  @Override
+  public int run(String[] args) throws Exception {
+
+    String inputPath = null;
+    String outputPath = null;
+    String configFile = null;
+    boolean overwrite = true;
+    int numReducers = 1;
+
+    // Read the command line arguments. We're not using GenericOptionsParser
+    // to prevent having to include commons.cli as a dependency.
+    for (int i = 0; i < args.length; i++) {
+      try {
+        if (args[i].equals(ARGNAME_INPATH)) {
+          inputPath = args[++i];
+        } else if (args[i].equals(ARGNAME_OUTPATH)) {
+          outputPath = args[++i];
+        } else if (args[i].equals(ARGNAME_CONF)) {
+          configFile = args[++i];
+        } else if (args[i].equals(ARGNAME_MAXFILES)) {
+          SampleFilter.setMax(Long.parseLong(args[++i]));
+        } else if (args[i].equals(ARGNAME_OVERWRITE)) {
+          overwrite = true;
+        } else if (args[i].equals(ARGNAME_NUMREDUCE)) {
+          numReducers = Integer.parseInt(args[++i]);
+        } else {
+          LOG.warn("Unsupported argument: " + args[i]);
+        }
+      } catch (ArrayIndexOutOfBoundsException e) {
+        usage();
+        throw new IllegalArgumentException();
+      }
+    }
+    
+    if (inputPath == null || outputPath == null) {
+      usage();
+      throw new IllegalArgumentException();
+    }
+
+    // Read in any additional config parameters.
+    if (configFile != null) {
+      LOG.info("adding config parameters from '"+ configFile + "'");
+      this.getConf().addResource(configFile);
+    }
+
+    // Create the Hadoop job.
+    Configuration conf = getConf();
+    conf.set("mapred.textoutputformat.separator",";");
+    
+    Job job = new Job(conf);
+    job.setJarByClass(ExampleTextWordCount.class);
+    job.setNumReduceTasks(numReducers);
+
+    // Scan the provided input path for ARC files.
+    LOG.info("setting input path to '"+ inputPath + "'");
+    SampleFilter.setFilter(FILEFILTER);
+    FileInputFormat.addInputPath(job, new Path(inputPath));
+    FileInputFormat.setInputPathFilter(job, SampleFilter.class);
+
+    // Delete the output path directory if it already exists and user wants to overwrite it.
+    if (overwrite) {
+      LOG.info("clearing the output path at '" + outputPath + "'");
+      FileSystem fs = FileSystem.get(new URI(outputPath), conf);
+      if (fs.exists(new Path(outputPath))) {
+        fs.delete(new Path(outputPath), true);
+      }
+    }
+    
+    // Set the path where final output 'part' files will be saved.
+    LOG.info("setting output path to '" + outputPath + "'");
+    FileOutputFormat.setOutputPath(job, new Path(outputPath));
+    FileOutputFormat.setCompressOutput(job, false);
+
+    // Set which InputFormat class to use.
+    job.setInputFormatClass(ArcInputFormat.class);
+
+    // Set which OutputFormat class to use.
+    job.setOutputFormatClass(TextOutputFormat.class);
+ 
+    // Set the output data types.
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(LongWritable.class);
+    
+    job.setJobName("UTwente - Managing Big Data - Linker");
+
+    job.setMapperClass(Linker.LinkerMapper.class);
+    job.setReducerClass(LongSumReducer.class);
+    job.setCombinerClass(LongSumReducer.class);
+
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new Linker(), args);
+    System.exit(res);
+  }
+}
